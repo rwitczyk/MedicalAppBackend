0f712e21c6e7c60a6c5de9d5a4623fd741dbd594
Robert Witczyk <rwitczyk@wp.pl>
2021-03-15 19:40:57 +0100

    Poprawki z dto.

--- src/main/java/dmcs/rwitczyk/dto/AddPatientAccountDto.java
+++ src/main/java/dmcs/rwitczyk/dto/AddPatientAccountDto.java
@@ -3,10 +3,12 @@ package dmcs.rwitczyk.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class AddPatientAccountDto {
 
     private String firstName;
--- src/main/java/dmcs/rwitczyk/dto/AvailableVisitsListDto.java
+++ src/main/java/dmcs/rwitczyk/dto/AvailableVisitsListDto.java
@@ -4,10 +4,12 @@ import dmcs.rwitczyk.models.VisitStatusEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class AvailableVisitsListDto {
 
     private int id;
